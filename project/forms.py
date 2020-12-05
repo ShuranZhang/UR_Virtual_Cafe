6cadfb812362a8f3245b4b74acdcded1b88c4769
JKGu <jgu8@u.rochester.edu>
2020-12-04 20:39:43 -0500

    adjust display order

--- project/forms.py
+++ project/forms.py
@@ -4,8 +4,9 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from project.models import User
+from wtforms.widgets import TextArea
 
 class PostForm(FlaskForm):
     title = StringField('Title',validators=[DataRequired()])
-    content = TextField('Content',validators=[DataRequired()])
+    content = StringField('Content',validators=[DataRequired()], widget=TextArea())
     submit = SubmitField('Post')
